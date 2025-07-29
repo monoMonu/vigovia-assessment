51c1259fa84e226532e95b6de7151801c0ff876a
monoMonu <ms88006218@gmail.com>
2025-07-29 13:58:30 +0000

    add api

--- src/components/ItineraryGenerator.jsx
+++ src/components/ItineraryGenerator.jsx
@@ -660,7 +660,7 @@ const ItineraryGenerator = () => {
                <div className='pt-8 px-5 text-center'>
                   {result ? 
                   <p>
-                     Genearted PDF location in file system : {result.url}
+                     Genearted PDF location in file system : <a href={result.url}></a>
                   </p> : 
                   <p>
                      Generated PDF Link will appear here...   
